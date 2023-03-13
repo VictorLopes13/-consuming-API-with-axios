2597d941231c7b2906334e86a851197b9f86eced
Victor Lopes <victorlopes@MacBook-Pro-de-Victor-2.local>
2023-03-13 16:49:21 -0300

    feat: initial project

---
+++ consumer-api/src/components/navBar/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+   <nav className="navbar">
+    <h2><Link to={'/'}>blog</Link></h2>
+    <ul>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/new'} className='new-btn'>new Post</Link></li>
+    </ul>
+   </nav>
+  )
+}
+
+export default NavBar
